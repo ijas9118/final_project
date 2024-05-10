e641269a778f6b8bfa34006d1b6aee8f7e1acf86
Ijas Ahammed <83322316+ijas9118@users.noreply.github.com>
2024-05-10 08:59:56 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -2,22 +2,24 @@ from ultralytics import YOLO
 from ultralytics.solutions import object_counter
 from flask_cors import CORS
 import cv2
-from flask import Flask, request, jsonify, Response
+from flask import Flask, json, request, jsonify
 from werkzeug.utils import secure_filename
 import os
-
 
 app = Flask(__name__)
 CORS(app)
 
-model = YOLO('./runs/detect/train/weights/best.pt')
+model = YOLO('./backend/YOLOv8/runs/detect/train/weights/best.pt')
 
 app.config['UPLOAD_FOLDER'] = './'
 
-@app.route('/upload', methods=['POST'])
+
+@app.route('/count_line', methods=['POST'])
 def upload_file():
     # Get the file from the request
     file = request.files['file']
+    selected_classes = json.loads(request.form['selectedClasses'])
+    confidence = float(request.form.get('confidence'))
 
     # Check if the file is present
     if file:
@@ -28,22 +30,22 @@ def upload_file():
         # Save the file
         file.save(file_path)
 
-        # Process the video file using YOLOv8
-        count_data = process_video(file_path)
+        # Process the video file using YOLOv8 with selected class IDs
+        count_data = count_line(file_path, selected_classes, confidence)
+        print(count_data)
         os.remove(file_path)
 
         return jsonify({'message': 'Video uploaded and processed successfully', 'count_data': count_data}), 200
     else:
         return jsonify({'error': 'No file uploaded'}), 400
 
-
-
-def process_video(filepath):
+def count_line(filepath, selected_classes, confidence):
     cap = cv2.VideoCapture(filepath)
     assert cap.isOpened(), "Error reading video file"
 
-    w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
     line_points = [(20, 400), (1080, 400)]
+    classes_to_count = [int(class_id) for class_id in selected_classes]  # Convert selected class IDs to integers
+    print(classes_to_count)
     counter = object_counter.ObjectCounter()
     counter.set_args(view_img=True, reg_pts=line_points, classes_names=model.names, draw_tracks=True, line_thickness=2)
 
@@ -53,7 +55,7 @@ def process_video(filepath):
             print("Video frame is empty or video processing has been successfully completed.")
             break
 
-        tracks = model.track(im0, persist=True, show=False)
+        tracks = model.track(im0, persist=True, show=False, classes=classes_to_count, conf=confidence)
         im0 = counter.start_counting(im0, tracks)
 
     cap.release()
@@ -62,6 +64,5 @@ def process_video(filepath):
     count_data = counter.class_wise_count
     return count_data
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
